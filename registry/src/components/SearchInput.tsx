2bc14b94bb1dd430f21e6cd2f9ae3693435c19da
Azlan <azlan@cuttilan.co.uk>
2026-08-30 17:49:31 +0100

    470 Form Submission Navigation Shortcut
    Signed-off-by: Azlan <azlan@cuttilan.co.uk>

--- registry/src/components/SearchInput.tsx
+++ registry/src/components/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Form } from "react-router-dom";
 
 export default function SearchInput() {
   const [term, setTerm] = useState('');
@@ -18,5 +18,11 @@ export default function SearchInput() {
         onChange={e => setTerm(e.target.value)}
       />
     </form>
-  )
+  );
+
+  // return (
+  //   <Form action="/search">
+  //     <input name="term" />
+  //   </Form>
+  // );
 }
