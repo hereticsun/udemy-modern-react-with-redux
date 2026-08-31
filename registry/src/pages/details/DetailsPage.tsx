624e0d0a788c97a9a8f7b57de76b83a720c78033
Azlan <azlan@cuttilan.co.uk>
2026-08-31 12:01:07 +0100

    487 Rendering Package Details
    Signed-off-by: Azlan <azlan@cuttilan.co.uk>

--- registry/src/pages/details/DetailsPage.tsx
+++ registry/src/pages/details/DetailsPage.tsx
@@ -5,8 +5,26 @@ export default function DetailsPage() {
   const { details } = useLoaderData() as DetailsLoaderResult;
 
   return (
-    <div>
-      {details.name} - {details.description}
+    <div className="space-y-4">
+      <h1 className="text3xl font-bold my-4">
+        {details.name}
+      </h1>
+
+      <div className="p-3 bg-gray-200 rounded">
+        <h3 className="text-lg font-bold">Description</h3>
+        {details.description}
+      </div>
+
+      <div className="p-3 bg-gray-200 rounded">
+        <h3 className="text-lg font-bold">License</h3>
+        {details.license}
+      </div>
+
+      <div className="p-3 bg-gray-200 rounded">
+        <h3 className="text-lg font-bold">Author</h3>
+        {details.author.name}
+      </div>
+
     </div>
   );
 }
